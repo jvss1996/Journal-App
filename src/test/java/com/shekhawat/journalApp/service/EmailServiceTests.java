c04147ad49d49cab68870aba7d77fc3195892dc8
Jaivardhan <jvss1996@gmail.com>
2024-11-14 18:07:46 +0530

    integrated JavaMailSender to send emails

---
+++ src/test/java/com/shekhawat/journalApp/service/EmailServiceTests.java
@@ -0,0 +1,17 @@
+package com.shekhawat.journalApp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTests {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    public void testSendEmail() {
+        emailService.sendEmail("jvss1996@gmail.com", "Testing java mail sender", "Hello, testing message");
+    }
+}
