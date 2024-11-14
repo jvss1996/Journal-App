c332d79d6a6af7e31fc5e505242b0569837da6e3
Jaivardhan <jvss1996@gmail.com>
2024-11-14 17:43:00 +0530

    integrated mongoTemplate to query filtered data

--- src/main/java/com/shekhawat/journalApp/entity/User.java
+++ src/main/java/com/shekhawat/journalApp/entity/User.java
@@ -1,5 +1,6 @@
 package com.shekhawat.journalApp.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -15,12 +16,15 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @Document(collection = "users")
+@AllArgsConstructor
 public class User {
     @Id
     private ObjectId Id;
     @Indexed(unique = true)
     @NonNull
     private String username;
+    private String email;
+    private boolean sentimentAnalysis;
     @NonNull
     private String password;
     @DBRef
---
+++ src/main/java/com/shekhawat/journalApp/repository/UserRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.shekhawat.journalApp.repository;
+
+import com.shekhawat.journalApp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
+public class UserRepositoryImpl {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public List<User> getUserForSA() {
+        Query query = new Query();
+        // check if email is valid
+        query.addCriteria(Criteria.where("email").regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}$"));
+        query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
+        List<User> users = mongoTemplate.find(query, User.class);
+        return users;
+    }
+}
